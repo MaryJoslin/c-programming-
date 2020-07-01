3e3f1a075702de27618d2c9687a30b794fccf1a9
MaryJoslin <67682464+MaryJoslin@users.noreply.github.com>
2020-07-01 14:32:00 +0530

    Created Project.cpp
    1. Write a c++ program to find the size of in flow chart and double. ( Solved )
    
    2.Write a c++ program to find the product of two numbers entered by the user. ( Solved)

---
+++ Project.cpp
@@ -0,0 +1,28 @@
+// A C++ program to find the size of int,float,double and char //
+
+
+#include<iostream>
+using namespace std;
+int main() 
+{    
+    cout<<"Size of char:"<<sizeof(char)<<" byte"<<endl;
+    cout<<"Size of int:"<<sizeof(int)<<" bytes"<<endl;
+    cout<<"Size of float:"<<sizeof(float)<<" bytes"<<endl;
+    cout<<"Size of double:"<<sizeof(double)<<" bytes"<<endl;
+    return 0;
+}
+
+
+// A C++ program to find the product of two numbers entered by the user //
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    double firstNumber,secondNumber,productOfTwoNumbers;
+    cout<<"Enter two numbers:";
+    cin>>firstNumber>> 
+    productOfTwoNumbers=firstNumber*secondNumber;  
+    cout<<"Product="<<productOfTwoNumbers;    
+    return 0;
+}
