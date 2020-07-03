87322d94bfaed0de97654d305380a19f59b9114f
MaryJoslin <67682464+MaryJoslin@users.noreply.github.com>
2020-07-03 18:34:37 +0530

    Update Project.cpp
    Project 1 : Solved

--- Project.cpp
+++ Project.cpp
@@ -15,14 +15,15 @@ int main()
 
 // A C++ program to find the product of two numbers entered by the user //
 
-#include<iostream>
+
+#include <iostream>
 using namespace std;
 int main()
 {
-    double firstNumber,secondNumber,productOfTwoNumbers;
-    cout<<"Enter two numbers:";
-    cin>>firstNumber>> 
-    productOfTwoNumbers=firstNumber*secondNumber;  
-    cout<<"Product="<<productOfTwoNumbers;    
+    double firstNumber, secondNumber, productOfTwoNumbers;
+    cout << "Enter two numbers: ";
+    cin >> firstNumber >> secondNumber;
+    productOfTwoNumbers = firstNumber * secondNumber;  
+    cout << "Product = " << productOfTwoNumbers;     
     return 0;
 }
